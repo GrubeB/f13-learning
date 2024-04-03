0b708c2f016ee615e10b1e21f73ecefd4011c5cd
GrubeB <dawidbladek0831@gmail.com>
2024-04-03 05:45:46 +0200

    service - leveling-service - config cors

--- service/learning/learning-service/src/main/java/pl/app/learning/config/WebMvcConfig.java
+++ service/learning/learning-service/src/main/java/pl/app/learning/config/WebMvcConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -38,6 +39,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-       registry.addMapping("/**");
+        registry
+                .addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS");
     }
 }
