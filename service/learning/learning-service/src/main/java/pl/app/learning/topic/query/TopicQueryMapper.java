fb3d4d1aa5cc2253b986c28fcad8b2a9211fc738
GrubeB <dawidbladek0831@gmail.com>
2024-04-16 10:38:55 +0200

    service - learning-service -  topic - bugfix

--- service/learning/learning-service/src/main/java/pl/app/learning/topic/query/TopicQueryMapper.java
+++ service/learning/learning-service/src/main/java/pl/app/learning/topic/query/TopicQueryMapper.java
@@ -3,6 +3,7 @@ package pl.app.learning.topic.query;
 import jakarta.annotation.PostConstruct;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.Converter;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.PropertyMap;
 import org.springframework.stereotype.Component;
@@ -10,9 +11,15 @@ import pl.app.common.ddd.AggregateId;
 import pl.app.common.mapper.BaseMapper;
 import pl.app.common.shared.dto.BaseDto;
 import pl.app.learning.category.query.CategoryQueryMapper;
+import pl.app.learning.category.query.dto.SimpleCategoryDto;
+import pl.app.learning.group.query.model.GroupHasCategoryQuery;
 import pl.app.learning.reference.query.ReferenceQueryMapper;
 import pl.app.learning.topic.query.dto.TopicDto;
+import pl.app.learning.topic.query.model.TopicHasCategoryQuery;
 import pl.app.learning.topic.query.model.TopicQuery;
+
+import java.util.List;
+import java.util.Set;
 
 @Getter
 @Component
@@ -27,6 +34,11 @@ public class TopicQueryMapper extends BaseMapper {
         modelMapper.addMappings(new PropertyMap<TopicQuery, TopicDto>() {
             @Override
             protected void configure() {
+                using((Converter<Set<TopicHasCategoryQuery>, List<SimpleCategoryDto>>) context -> context.getSource().stream()
+                        .map(TopicHasCategoryQuery::getCategory)
+                        .map(c -> modelMapper.map(c, SimpleCategoryDto.class))
+                        .toList()
+                ).map(source.getCategories()).setCategories(null);
                 map(source.getComment().getComments()).setComments(null);
                 map(source.getReference().getReferences()).setReferences(null);
                 map(source.getProgress().getProgresses()).setProgresses(null);
